652152b0ad3f9d4534935ca5b707f243e36b494d
salindah <h.salinda@gmail.com>
2020-07-03 23:38:40 +0200

    Re structured the application with more granular components.

---
+++ react-demo/src/components/Cockpit/Cockpit.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import classes from './Cockpit.module.css';
+
+const cockpit = (props) => {
+
+    let buttonClass = '';
+    if (props.showPersons) {
+        buttonClass = classes.Red;
+    }
+
+
+    let assignedClasses = [];
+    if (props.persons.length <= 2) {
+        assignedClasses.push(classes.red);
+    }
+    if (props.persons.length <= 1) {
+        assignedClasses.push(classes.bold);
+    }
+
+    return(
+        <div className={classes.Cockpit}>
+            <h1>Hi I am a react app</h1>
+            <p className={assignedClasses.join(' ')} > This is really working </p>
+            <button className={buttonClass}
+                onClick={props.clicked} > Toggle Persons
+            </button>
+        </div>
+    );
+}
+
+export default cockpit;
---
+++ react-demo/src/containers/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+
+class App extends Component {
+
+  state = {
+    persons: [
+      { id: "21312312", name: "Sali", age: 33 },
+      { id: "34534534", name: "Iesh", age: 28 },
+      { id: "67675656", name: "Aradhya", age: 5 },
+      { id: "12122222", name: "Aarya", age: 1 }
+    ],
+    showPersons: false
+  };
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    person.name = event.target.value;
+
+    const personsUpdated = [...this.state.persons];
+    personsUpdated[personIndex] = person;
+    this.setState({ persons: personsUpdated });
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({
+      persons: persons
+    });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    });
+  }
+
+  render() {
+
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = <Persons
+                  persons={this.state.persons}
+                  clicked={this.deletePersonHandler}
+                  changed={this.nameChangedHandler} />
+    }
+
+    return (
+      <div className={classes.App}>
+        <Cockpit 
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler}/>
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
