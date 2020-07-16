bec32ae321ee0df1c5150728d8a6d33aae78bac1
salindah <h.salinda@gmail.com>
2020-07-16 23:34:21 +0200

    Adding axious global configuration, and POST request handler

--- react-web/src/containers/Blog/Blog.js
+++ react-web/src/containers/Blog/Blog.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false
+    };
+
+    componentDidMount() {
+        axios.get('/posts')
+            .then(response => {
+
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+
+                this.setState({
+                    posts: updatedPosts
+                });
+                console.log(updatedPosts);
+            })
+            .catch( error => {
+                console.log(error);
+                this.setState({error: true});
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
+    render() {
+        let posts = <p style={{textAlign: 'center', color:'red'}}>Something went wrong!</p>
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    author={post.author}
+                    title={post.title}
+                    clicked={() => this.postSelectedHandler(post.id)} />;
+            });
+        }
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
--- react-web/src/index.js
+++ react-web/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH_TOKEN';
+
+
+axios.interceptors.request.use( request => {
+    console.log(request);
+    return request;
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
