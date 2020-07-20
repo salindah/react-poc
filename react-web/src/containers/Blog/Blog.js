c41b406f10108faa4b26f58b20371c245ce63edb
salindah <h.salinda@gmail.com>
2020-07-20 18:56:30 +0200

    Added two different routes to the page

--- react-web/src/containers/Blog/Blog.js
+++ react-web/src/containers/Blog/Blog.js
@@ -1,30 +1,25 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
+
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-
-
+    
     render() {
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts />
-                
-                {/* <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section> */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
--- react-web/src/containers/Blog/Posts/Posts.js
+++ react-web/src/containers/Blog/Posts/Posts.js
@@ -15,6 +15,8 @@ class Posts extends Component {
     }
 
     componentDidMount() {
+
+        console.log(this.props);
         axios.get('/posts')
             .then(response => {
 
