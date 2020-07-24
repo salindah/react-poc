85959123ac0d0b1fd2e835a9ee3ec32b58b927ae
salindah <h.salinda@gmail.com>
2020-07-24 19:01:54 +0200

    Adding component lazy loading imports

--- react-web/src/containers/Blog/Blog.js
+++ react-web/src/containers/Blog/Blog.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
 import './Blog.css';
+
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+} );
 
 class Blog extends Component {
 
     state = {
-        auth: false
+        auth: true
     };
 
     render() {
@@ -23,7 +29,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/post" component={Posts} />
 
                     <Route render={() => <h1>Not Found</h1>} />
